0b2dc7436b392193d7b65cb061e6ceb3cfde763c
ericpedley@gmail.com <ericpedley@gmail.com>
2020-10-17 23:14:29 -0700

    added predictions on web server and addded looping on browser

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import cv2
 from base64 import b64decode
 from matplotlib import pyplot as plt
 
+width=80
+height=60
+predicting=True
+if predicting:
+    from model_example import make_model
+    model = make_model(input_shape=(width,height) + (1,), num_classes=2)
+    model.load_weights("save_at_10.h5")
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,15 +21,20 @@ def index():
 def frame():
     json = request.get_json()
     mask = np.array(list(json["mask"].items()))
-    mask = mask[:,1].reshape(480,640).astype(np.int)
-    mask=np.expand_dims(mask,2)
+    mask = mask[:,1].reshape(480,640).astype(np.uint8)
     header,img_encoded = str(json["img"]).split(",",1)
     img_raw = b64decode(img_encoded)
     img = np.fromstring(img_raw,np.uint8)
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    combined = np.multiply(img,mask)*255
-    cv2.imshow("bruh",combined)
-    cv2.waitKey(0)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    combined = np.multiply(img,mask)
+    frame = cv2.resize(combined,(width,height),interpolation=cv2.INTER_AREA)
+    if predicting:
+            frame=np.expand_dims(frame,axis=2)
+            frame=np.expand_dims(frame,axis=0)
+            frame=np.swapaxes(frame,1,2)
+            final_prediction=model.predict(frame)[0][0]
+            return str(final_prediction)
     return "got it"
     
 if __name__ == '__main__':
